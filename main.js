1df7cecdc41f1fa2b8a3f47fdc8d949a704507f6
raul reymundi <kinskispiral@gmail.com>
2022-05-23 23:02:44 -0400

    commit tree fbb13350cbe44d93e40b0a280500b52f7d659052

--- main.js
+++ main.js
@@ -4,6 +4,37 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+// server request
+const hearts = document.querySelectorAll(".like-glyph")
+
+
+// for (const span of hearts) {
+  span.addEventListener("click", heartLiker);
+// }
+
+function heartLiker(e) {
+  const heart = e.target;
+  mimicServerCall("someURL")
+    .then(() => {
+      if (heart.innerText === EMPTY_HEART) {
+        heart.innerText = FULL_HEART;
+        heart.className = "activated-heart";
+      } else {
+        heart.innerText = EMPTY_HEART;
+        heart.className = "";
+      }
+    })
+    .catch((error) => {
+      const modal = document.getElementById("modal");
+      modal.className = "";
+      modal.innerText = error;
+      setTimeout(() =>  modal.className = "hidden", 3000);
+    });
+}
+
+// for (const span of hearts) {
+//   span.addEventListener("click", heartLiker);
+// }
 
 
 
